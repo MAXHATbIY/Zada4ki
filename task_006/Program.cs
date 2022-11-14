411f16db4f9beec7bd9ff9600f915c4a4804cb24
Slava <mc_slava2@mail.ru>
2022-11-14 21:37:33 +0300

    add 006 SumDivProDif

---
+++ task_006/Program.cs
@@ -0,0 +1,17 @@
+﻿// Даны два действительных числа x и у. 
+// Вычислить их сумму, разность, произведение и частное.
+double x;
+double y;
+
+Console.WriteLine("Введите число: ");
+x = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+y = Convert.ToDouble(Console.ReadLine());
+
+double Sum = x + y;
+double Dif = x - y;
+double Pro = x * y;
+double Div = x / y;
+Div = Math.Round(Div,2);
+
+Console.WriteLine($"Сумма: {Sum}; Разность: {Dif}; Произведение: {Pro}; Частное: {Div}");
